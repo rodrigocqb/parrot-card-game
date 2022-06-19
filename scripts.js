0f34f600a0e3213d191d49bc07080291471dc2f0
rodrigocqb <rodrigocortibarros@gmail.com>
2022-06-19 15:43:32 -0300

    Cartas não podem ser clicadas antes das outras desvirarem

--- scripts.js
+++ scripts.js
@@ -9,8 +9,8 @@ for (let i = 0; i < pares; i++) {
     jogo.push(tipos[i]);
     jogo.push(tipos[i]);
 }
-jogo.sort(() => {return Math.random() - 0.5});
-for (let i = 0; i < jogo.length; i++){
+jogo.sort(() => { return Math.random() - 0.5 });
+for (let i = 0; i < jogo.length; i++) {
     const tabuleiro = document.querySelector(".tabuleiro");
     tabuleiro.innerHTML += `
     <div class="carta" onclick="viraCarta(this)">
@@ -22,33 +22,42 @@ for (let i = 0; i < jogo.length; i++){
         </div>
     </div>`
 }
-let cliques = 0
+let cliques = 0;
 let primeira;
-let segunda
-function viraCarta(elemento){
-    if (cliques%2==0){
-        primeira = elemento;
-        primeira.querySelector(".frente").classList.add("escondido");
-        primeira.querySelector(".traseiro").classList.remove("escondido");
-        cliques++;
-    } else {
-        segunda = elemento;
-        segunda.querySelector(".frente").classList.add("escondido");
-        segunda.querySelector(".traseiro").classList.remove("escondido");
-        if (primeira.innerHTML == segunda.innerHTML){
-            primeira = undefined;
-            segunda = undefined;
+let segunda;
+let duasViradas = false;
+function viraCarta(elemento) {
+    if (duasViradas === false) {
+        if (cliques % 2 == 0) {
+            primeira = elemento;
+            primeira.querySelector(".frente").classList.add("escondido");
+            primeira.querySelector(".traseiro").classList.remove("escondido");
+            cliques++;
+            console.log(cliques)
         } else {
-            setTimeout(()=>{
-            primeira.querySelector(".traseiro").classList.add("escondido");
-            primeira.querySelector(".frente").classList.remove("escondido");
-            segunda.querySelector(".traseiro").classList.add("escondido");
-            segunda.querySelector(".frente").classList.remove("escondido");
-            },1000);
+            segunda = elemento;
+            segunda.querySelector(".frente").classList.add("escondido");
+            segunda.querySelector(".traseiro").classList.remove("escondido");
+            if (primeira.innerHTML == segunda.innerHTML) {
+                primeira = undefined;
+                segunda = undefined;
+            } else {
+                duasViradas = true;
+                setTimeout(() => {
+                    primeira.querySelector(".traseiro").classList.add("escondido");
+                    primeira.querySelector(".frente").classList.remove("escondido");
+                    segunda.querySelector(".traseiro").classList.add("escondido");
+                    segunda.querySelector(".frente").classList.remove("escondido");
+                    duasViradas = false;
+                }, 1000);
+            }
+            cliques++;
+            console.log(cliques)
         }
-        cliques++;
     }
-    if (document.querySelectorAll(".frente.escondido").length == jogo.length){
-        alert(`Você ganhou em ${cliques} jogadas!`);
+    if (document.querySelectorAll(".frente.escondido").length == jogo.length) {
+        setTimeout(() => {
+            alert(`Você ganhou em ${cliques} jogadas!`);
+        },300);
     }
 }
