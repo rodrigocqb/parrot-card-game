134759c3a2e27a3aebefac3fcc9726d96762d7d7
rodrigocqb <rodrigocortibarros@gmail.com>
2022-06-19 23:00:32 -0300

    Função de reiniciar jogo implementada

--- scripts.js
+++ scripts.js
@@ -1,18 +1,21 @@
-let cartas = prompt("Quantas cartas você quer no jogo?");
-while (cartas % 2 != 0 || cartas < 4 || cartas > 14) {
-    cartas = prompt("Insira um número par entre 4 e 14")
-}
-const pares = cartas / 2;
-const jogo = [];
-const tipos = [["./img/revertitparrot.gif"], ["./img/bobrossparrot.gif"], ["./img/explodyparrot.gif"], ["./img/fiestaparrot.gif"], ["./img/metalparrot.gif"], ["./img/tripletsparrot.gif"], ["./img/unicornparrot.gif"]];
-for (let i = 0; i < pares; i++) {
-    jogo.push(tipos[i]);
-    jogo.push(tipos[i]);
-}
-jogo.sort(() => { return Math.random() - 0.5 });
-for (let i = 0; i < jogo.length; i++) {
-    const tabuleiro = document.querySelector(".tabuleiro");
-    tabuleiro.innerHTML += `
+let jogo;
+start();
+function start() {
+    jogo = [];
+    let cartas = prompt("Quantas cartas você quer no jogo?");
+    while (cartas % 2 != 0 || cartas < 4 || cartas > 14) {
+        cartas = prompt("Insira um número par entre 4 e 14")
+    }
+    const pares = cartas / 2;
+    const tipos = [["./img/revertitparrot.gif"], ["./img/bobrossparrot.gif"], ["./img/explodyparrot.gif"], ["./img/fiestaparrot.gif"], ["./img/metalparrot.gif"], ["./img/tripletsparrot.gif"], ["./img/unicornparrot.gif"]];
+    for (let i = 0; i < pares; i++) {
+        jogo.push(tipos[i]);
+        jogo.push(tipos[i]);
+    }
+    jogo.sort(() => { return Math.random() - 0.5 });
+    for (let i = 0; i < jogo.length; i++) {
+        const tabuleiro = document.querySelector(".tabuleiro");
+        tabuleiro.innerHTML += `
     <div class="carta" onclick="viraCarta(this)">
         <div class="frente">
             <img src="./img/front.png">
@@ -21,6 +24,7 @@ for (let i = 0; i < jogo.length; i++) {
             <img src=${jogo[i]}>
         </div>
     </div>`
+    }
 }
 let cliques = 0;
 let primeira;
@@ -28,7 +32,7 @@ let segunda;
 let duasViradas = false;
 function viraCarta(elemento) {
     if (duasViradas === false) {
-        if (elemento.querySelector(".back").classList.contains("escondido")===false){
+        if (elemento.querySelector(".back").classList.contains("escondido") === false) {
             return;
         }
         if (cliques % 2 == 0) {
@@ -63,6 +67,11 @@ function viraCarta(elemento) {
     if (document.querySelectorAll(".frente.escondido").length == jogo.length) {
         setTimeout(() => {
             alert(`Você ganhou em ${cliques} jogadas!`);
-        },400);
+            const restart = prompt("Você gostaria de jogar novamente? Digite 'SIM' ou 'NÃO'");
+            if (restart == "SIM") {
+                document.querySelector(".tabuleiro").innerHTML="";
+                start();
+            }
+        }, 400);
     }
 }
